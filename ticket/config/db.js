dc4e7d8d56422cda4f6da82c74f07a9602a94f2f
oudia15@hotmail.fr <oudia15@hotmail.fr>
2018-02-14 00:20:02 -0600

    deleted initial ticket folder

--- ticket/config/db.js
+++
@@ -1,6 +0,0 @@
-const crypto = require('crypto');
-
-module.exports = {
-  uri: 'mongodb://localhost:27017/meanauth',
-  secret: crypto.randomBytes(256).toString('hex')
-}
--- ticket/helpers/log.js
+++
@@ -1,3 +0,0 @@
-module.exports = (argument) => {
-  console.log(argument);
-}
